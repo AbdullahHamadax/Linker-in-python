dff0a7ecfa42e91c7b8f488c686ae1c2cd0c2652
abdomosa194 <abodeymousa@gmail.com>
2024-12-21 13:40:17 +0200

    add comment

--- linker.py
+++ linker.py
@@ -16,29 +16,29 @@ class Linker:
         self.symbol_table = SymbolTable()
         self.linked_code = []
 
-    def load_object_file(self, filename):
+    def load_object_file(self, filename): #To load an object file and extract its code and symbols.
         """This is a doctring to improve our code quality """
-        code, symbols = parse_object_file(filename)
-        obj_file = ObjectFile(filename, code, symbols)
-        for symbol, address in obj_file.symbols.items():
+        code, symbols = parse_object_file(filename)     # returns the "code" and "symbols" extracted from the object file in utlis.py.
+        obj_file = ObjectFile(filename, code, symbols)  # Creates an "ObjectFile" instance using the "filename", "code", and "symbols".
+        for symbol, address in obj_file.symbols.items(): # "Add each symbol from the object file to the symbol table with its corresponding address."
             self.symbol_table.add_symbol(symbol, address)
         return obj_file
 
-    def link(self, object_files):
+    def link(self, object_files): # Resolve symbols and combine code from multiple object files.
         """This is a doctring to improve our code quality """
-        for obj_file in object_files:
+        for obj_file in object_files: 
             for line in obj_file.code:
                 resolved_line = self.resolve_symbols(line)
                 self.linked_code.append(resolved_line)
         return self.linked_code
 
-    def resolve_symbols(self, line):
+    def resolve_symbols(self, line): # Replace symbol names in the code line with their memory addresses.
         """This is a doctring to improve our code quality """
         for symbol, address in self.symbol_table.symbols.items():
             line = line.replace(symbol, str(address))
         return line
 
-    def save_output(self, output_filename):
+    def save_output(self, output_filename):# Write the linked code to the specified output file.
         """This is a doctring to improve our code quality """
         os.makedirs(os.path.dirname(output_filename), exist_ok=True)
         with open(output_filename, 'w') as f:
